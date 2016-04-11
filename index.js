91e4bc7b2ac5213e7800b3dad263c47ef9f679f1
amith <amith@amiths-MacBook-Pro-2.local>
2016-04-11 14:17:28 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var fs = require("fs");
+var parseRange = require("range-parser");
+var mkdirp = require("mkdirp");
+var path = require('path')
+
+exports.readRange = function(fd, range, callback) {
+  fs.stat(fd, function(err, stat) {
+    if(err) {
+      callback(err, null);
+    } else {
+      var fileSize = stat.size;
+      var parsedRange = parseRange(fileSize, range);
+      if(parsedRange === -1 || parsedRange === -2) {
+        callback(null, -1);
+      } else {
+        var start = parsedRange[0].start;
+        var end = parsedRange[0].end;
+        var length = end - start + 1;
+        var buffer = new Buffer(length);
+        fs.read(fd, buffer, 0, length, start, function(err, data) {
+          if(err) {
+            callback(err, null);
+          } else {
+            callback(null, data);
+          }
+        });
+      }
+    }
+  });
+};
+
+exports.ensureFile = function(fd, flags, mode, callback) {
+  var dirname = path.dirname(fd);
+  if (typeof mode == 'function') {
+        callback = mode;
+        mode = 777;
+  }
+  mkdirp(dirname, mode, function(err,data) {
+    if(err) {
+      callback(err, null);
+    } else {
+      fs.open(fd, flags, callback);
+    }
+  });
+};
+
+exports.ensureCopy = function(source, dest, mode, callback) {
+  var dirname = path.dirname(dest);
+  if (typeof mode == 'function') {
+        callback = mode;
+        mode = 777;
+  }
+  mkdirp(dirname, mode, function(err,data) {
+    if(err) {
+      callback(err, null);
+    } else {
+      var sourceStream = fs.createReadStream(source);
+      var destStream = fs.createWriteStream(dest);
+      sourceStream.on("error", function(err) {
+        callback(err, null);
+      });
+      destStream.on("error", function(err) {
+        callback(err, null);
+      });
+      destStream.on("finish", function(err, data) {
+        callback(null, dest);
+      });
+    }
+  });
+};
